b2a1c9643b4447bf8db1942ff0c86b0e76491f91
Christian Kellner <christian.kellner@instana.com>
2020-02-25 08:19:05 +0100

    adding eslint and husky to prevent bs commits

--- index.js
+++ index.js
@@ -5,27 +5,25 @@ const config = require('./conf/config.json');
 const FredyRuntime = require('./lib/FredyRuntime');
 
 setInterval(
-    (function exec() {
-        Object.keys(config.jobs).forEach(jobKey => {
-            const jobConfig = config.jobs[jobKey];
-            provider
-                .map(pro => require(`${path}/${pro}`))
-                .forEach(pro => {
-                    const providerId = pro.id();
-                    if (providerId == null || providerId.length === 0) {
-                        throw new Error('Provider id must not be empty. => ' + pro);
-                    }
-                    const providerConfig = jobConfig.provider[providerId];
-                    if (providerConfig == null) {
-                        throw new Error("Provider Config for provider with id " + providerId + " not found.");
-                    }
-                    pro.init(providerConfig, jobConfig.blacklist, jobConfig.blacklistedDistricts);
-                    new FredyRuntime(pro.config, jobConfig.notification, providerId, jobKey)
-                        .execute();
-                })
-
+  (function exec() {
+    Object.keys(config.jobs).forEach(jobKey => {
+      const jobConfig = config.jobs[jobKey];
+      provider
+        .map(pro => require(`${path}/${pro}`))
+        .forEach(pro => {
+          const providerId = pro.id();
+          if (providerId == null || providerId.length === 0) {
+            throw new Error('Provider id must not be empty. => ' + pro);
+          }
+          const providerConfig = jobConfig.provider[providerId];
+          if (providerConfig == null) {
+            throw new Error(`Provider Config for provider with id ${providerId} not found.`);
+          }
+          pro.init(providerConfig, jobConfig.blacklist, jobConfig.blacklistedDistricts);
+          new FredyRuntime(pro.config, jobConfig.notification, providerId, jobKey).execute();
         });
-        return exec;
-    })(),
-    config.interval * 60 * 1000
+    });
+    return exec;
+  })(),
+  config.interval * 60 * 1000
 );
