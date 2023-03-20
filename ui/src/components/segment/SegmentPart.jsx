68220babfb72f0f4cfcf4156364cf475c5859e29
Christian Kellner <Christian.Kellner1@ibm.com>
2023-03-20 08:51:20 +0100

    removing comments

--- ui/src/components/segment/SegmentPart.jsx
+++ ui/src/components/segment/SegmentPart.jsx
@@ -16,27 +16,3 @@ export const SegmentPart = ({ name, Icon = null, children, helpText }) => {
     </Card>
   );
 };
-
-/*
-
-  <Segment inverted>
-    <Header as="h5" inverted sub>
-      {icon && <Icon name={icon} inverted size="mini" />}
-      <Header.Content>{name}</Header.Content>
-    </Header>
-
-    <Popup
-      content={helpText}
-      trigger={
-        <span className="generalSettings__help">
-          {' '}
-          <Icon name="help circle" inverted />
-          What is this?
-        </span>
-      }
-    />
-    <Segment inverted className="segmentParts">
-      {children}
-    </Segment>
-  </Segment>
- */
