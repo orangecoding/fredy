3562b66c30528136331330fd363b75eb726deb42
Patrick Sean Klein <patrick@libklein.com>
2025-04-27 15:15:58 +0100

    Implement provider test.

--- test/provider/immoscout-mobile.test.js
+++ test/provider/immoscout-mobile.test.js
@@ -1,7 +1,45 @@
 import {expect} from 'chai';
 import {convertWebToMobile} from '../../lib/provider/immoscout-mobile.js';
+import * as similarityCache from '../../lib/services/similarity-check/similarityCache.js';
+import {mockFredy, providerConfig} from '../utils.js';
+import {get} from '../mocks/mockNotification.js';
+import * as provider from '../../lib/provider/immoscout-mobile.js';
 
-describe('#immoscout-mobile testsuite()', () => {
+describe('#immoscout-mobile provider testsuite()', () => {
+    after(() => {
+        similarityCache.stopCacheCleanup();
+    });
+
+    provider.init(providerConfig.immoscout, [], []);
+    it('should test immoscout-mobile provider', async () => {
+        const Fredy = await mockFredy();
+        return await new Promise((resolve) => {
+            const fredy = new Fredy(provider.config, null, provider.metaInformation.id, '', similarityCache);
+            fredy.execute().then((listings) => {
+                expect(listings).to.be.a('array');
+                const notificationObj = get();
+                expect(notificationObj).to.be.a('object');
+                expect(notificationObj.serviceName).to.equal('immoscout-mobile');
+                notificationObj.payload.forEach((notify) => {
+                    /** check the actual structure **/
+                    expect(notify.id).to.be.a('string');
+                    expect(notify.price).to.be.a('string');
+                    expect(notify.size).to.be.a('string');
+                    expect(notify.title).to.be.a('string');
+                    expect(notify.link).to.be.a('string');
+                    expect(notify.address).to.be.a('string');
+                    /** check the values if possible **/
+                    expect(notify.size).to.be.not.empty;
+                    expect(notify.title).to.be.not.empty;
+                    expect(notify.link).that.does.include('https://www.immobilienscout24.de/');
+                });
+                resolve();
+            });
+        });
+    });
+});
+
+describe('#immoscout-mobile URL conversion', () => {
     // Test URL conversion
     it('should convert a full web URL to mobile URL', () => {
         const webUrl = 'https://www.immobilienscout24.de/Suche/de/berlin/berlin/wohnung-mieten?heatingtypes=central,selfcontainedcentral&haspromotion=false&numberofrooms=2.0-5.0&livingspace=10.0-25.0&energyefficiencyclasses=a,b,c,d,e,f,g,h,a_plus&exclusioncriteria=projectlisting,swapflat&equipment=parking,cellar,builtinkitchen,lift,garden,guesttoilet,balcony&petsallowedtypes=no,yes,negotiable&price=10.0-100.0&constructionyear=1920-2026&apartmenttypes=halfbasement,penthouse,other,loft,groundfloor,terracedflat,raisedgroundfloor,roofstorey,apartment,maisonette&pricetype=calculatedtotalrent&floor=2-7&enteredFrom=result_list';
