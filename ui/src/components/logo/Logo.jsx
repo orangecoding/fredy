89d239c360fb768fe430e446a4fd787a833819dc
Christian Kellner <weakmap@gmail.com>
2025-09-25 15:03:47 +0200

    New Listings view (#192)
    * completing found listings
    
    ---------
    
    Co-authored-by: Christian Kellner <Christian.Kellner1@ibm.com>

--- ui/src/components/logo/Logo.jsx
+++ ui/src/components/logo/Logo.jsx
@@ -5,5 +5,5 @@ import logoWhite from '../../assets/logo_white.png';
 import './Logo.less';
 
 export default function Logo({ width = 350, white = false } = {}) {
-  return <img src={white ? logoWhite : logo} width={width} className="logo" />;
+  return <img src={white ? logoWhite : logo} width={width} className="logo" alt="Fredy Logo" />;
 }
