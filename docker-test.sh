9dd3947cb7c6c1cee4bdeab561b56d40115aff3b
orangecoding <weakmap@gmail.com>
2025-10-02 09:37:01 +0200

    reverting docker file changes, adding script to test things locally

---
+++ docker-test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# Stop and remove old container if it exists
+if [ "$(docker ps -aq -f name=fredy)" ]; then
+  docker stop fredy || true
+  docker rm fredy || true
+fi
+
+# Build image from local Dockerfile
+docker build -t fredy:local .
+
+# Run container with volumes and port mapping
+docker run -d --name fredy \
+  -v fredy_conf:/conf \
+  -v fredy_db:/db \
+  -p 9998:9998 \
+  fredy:local
