fed77be94de4cf4e8f53b84b1c6a6e99cecd526c
strech345 <tantenklatsch@web.de>
2026-03-16 21:06:11 +0100

    feat(): add types, update providers, they now return specs as numbers

--- test/provider/einsAImmobilien.test.js
+++ test/provider/einsAImmobilien.test.js
@@ -10,7 +10,7 @@ import { expect } from 'chai';
 import * as provider from '../../lib/provider/einsAImmobilien.js';
 
 describe('#einsAImmobilien testsuite()', () => {
-  provider.init(providerConfig.einsAImmobilien, [], []);
+  provider.init(providerConfig.einsAImmobilien, []);
   it('should test einsAImmobilien provider', async () => {
     const Fredy = await mockFredy();
     const mockedJob = {
@@ -20,28 +20,23 @@ describe('#einsAImmobilien testsuite()', () => {
       specFilter: null,
     };
 
-    return await new Promise((resolve) => {
-      const fredy = new Fredy(provider.config, mockedJob, provider.metaInformation.id, similarityCache);
-
-      fredy.execute().then((listings) => {
-        expect(listings).to.be.a('array');
-        const notificationObj = get();
-        expect(notificationObj).to.be.a('object');
-        expect(notificationObj.serviceName).to.equal('einsAImmobilien');
-        notificationObj.payload.forEach((notify) => {
-          /** check the actual structure **/
-          expect(notify.id).to.be.a('string');
-          expect(notify.price).to.be.a('string');
-          expect(notify.size).to.be.a('string');
-          expect(notify.title).to.be.a('string');
-          expect(notify.link).to.be.a('string');
-          expect(notify.address).to.be.a('string');
-          /** check the values if possible **/
-          expect(notify.size).to.be.not.empty;
-          expect(notify.title).to.be.not.empty;
-          expect(notify.link).that.does.include('https://www.1a-immobilienmarkt.de');
-        });
-        resolve();
+    const fredy = new Fredy(provider.config, mockedJob, provider.metaInformation.id, similarityCache);
+    return fredy.execute().then((listings) => {
+      expect(listings).to.be.a('array');
+      const notificationObj = get();
+      expect(notificationObj).to.be.a('object');
+      expect(notificationObj.serviceName).to.equal('einsAImmobilien');
+      notificationObj.payload.forEach((notify) => {
+        /** check the actual structure **/
+        expect(notify.id).to.be.a('string');
+        expect(notify.price).to.be.a('number');
+        expect(notify.size).to.be.a('number');
+        expect(notify.title).to.be.a('string');
+        expect(notify.link).to.be.a('string');
+        expect(notify.address).to.be.a('string');
+        /** check the values if possible **/
+        expect(notify.title).to.be.not.empty;
+        expect(notify.link).that.does.include('https://www.1a-immobilienmarkt.de');
       });
     });
   });
