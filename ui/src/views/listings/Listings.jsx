fef9901c9ddc0bd3a0c1b76baf6db5396af2158b
datenwurm <git@datenwurm.net>
2026-05-06 07:46:50 +0000

    feat: Add grid/table view toggle to listings overview

--- ui/src/views/listings/Listings.jsx
+++ ui/src/views/listings/Listings.jsx
@@ -3,14 +3,14 @@
  * Licensed under Apache-2.0 with Commons Clause and Attribution/Naming Clause
  */
 
-import ListingsGrid from '../../components/grid/listings/ListingsGrid.jsx';
+import ListingsOverview from '../../components/listings/ListingsOverview.jsx';
 import Headline from '../../components/headline/Headline.jsx';
 
 export default function Listings() {
   return (
     <>
       <Headline text="Listings" />
-      <ListingsGrid />
+      <ListingsOverview />
     </>
   );
 }
